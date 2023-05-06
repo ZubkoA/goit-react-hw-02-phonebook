cab0b646f3ba1e108dd9c8f3f230b2f09c7ccb44
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-05-06 15:01:59 +0300

    second task done

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const ContactList = ({ contacts }) => (
   <ul className="contacts">
-    {contacts.map(({ name, id }) => (
+    {contacts.map(({ name, id, number }) => (
       <li className="contacts__item" key={id}>
-        <p className="contacts__text">{name}</p>
+        <p className="contacts__text">
+          {name}: {number}
+        </p>
       </li>
     ))}
   </ul>
